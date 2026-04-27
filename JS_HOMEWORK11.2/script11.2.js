861bf276c9bcfc0f00362082c959aab42eb0d65c
Artemiy K <kurbatovartur40@gmail.com>
2026-04-27 15:21:31 +0300

    Update script11.2.js files

--- JS_HOMEWORK11.2/script11.2.js
+++ JS_HOMEWORK11.2/script11.2.js
@@ -1,15 +1,7 @@
 const text = document.querySelector("#text");
 const btn = document.querySelector("#btn");
-text.classList.add("blacktext");
-let isGreen = false;
+
 btn.addEventListener("click", function () {
-    if (isGreen) {
-        text.classList.remove("green");
-        text.classList.add("blacktext");
-        isGreen = false;
-    } else {
-        text.classList.remove("blacktext");
-        text.classList.add("green");
-        isGreen = true;
-    }
+    text.classList.toggle("green");
+    text.classList.toggle("blacktext");
 });
