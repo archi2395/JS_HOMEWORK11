0894560aec243a685902f509838e42738375fcc1
Artemiy K <kurbatovartur40@gmail.com>
2026-04-27 15:01:45 +0300

    Update files

--- JS_HOMEWORK11.2/script11.2.js
+++ JS_HOMEWORK11.2/script11.2.js
@@ -1,11 +1,7 @@
 const text = document.querySelector("#text");
 const btn = document.querySelector("#btn");
-
-// ставимо початковий клас
 text.classList.add("blacktext");
-
 let isGreen = false;
-
 btn.addEventListener("click", function () {
     if (isGreen) {
         text.classList.remove("green");
